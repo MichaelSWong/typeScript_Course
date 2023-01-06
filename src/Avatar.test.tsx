d51cba7d06336829762b72fa78b7afc3d7e24f57
Michael Wong <mizik_jah@yahoo.com>
2023-01-06 17:07:28 -0500

    added fallback tests

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -1,5 +1,5 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, it, beforeEach } from 'vitest';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 
 import Avatar, {
   FALLBACK_AVATAR_ALT_TEXT,
@@ -7,6 +7,9 @@ import Avatar, {
 } from './Avatar';
 
 describe('Avatar', () => {
+  beforeEach(() => {
+    cleanup();
+  });
   it('should render an <img />', () => {
     const url = 'https://cataas.com/cat/says/hello%20world!';
     const alt = '@github-handle';
@@ -26,6 +29,19 @@ describe('Avatar', () => {
   it('should use a fallback image if no URL passed in', () => {
     render(<Avatar />);
     const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL);
+  });
+  it('should use a fallback image if image fails to load', async () => {
+    render(<Avatar url="https://hello.com/fake.png" />);
+    const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    fireEvent.error(img);
+    expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL);
+  });
+  it('should use a fallback image if url is empty string', () => {
+    render(<Avatar url="" />);
+
+    const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    fireEvent.error(img);
     expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL);
   });
 });
--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 export const FALLBACK_AVATAR_URL = 'https://cataas.com/cat/says/hello%20world!';
 export const FALLBACK_AVATAR_ALT_TEXT = '@hello-cat';
 
@@ -7,11 +8,25 @@ type AvatarProps = {
   /** the alt text to use for the url image */
   alt?: string;
 };
+
+// NOTES
+// if onError is called, useFallback, url and alt text
+// 1. useState
+// 2. update src and alt when onError is called
 const Avatar = ({
   url = FALLBACK_AVATAR_URL,
   alt = FALLBACK_AVATAR_ALT_TEXT,
 }: AvatarProps) => {
-  return <img src={url} alt={alt} />;
+  const [srcToRender, setSrcToRender] = useState(url);
+  return (
+    <img
+      src={srcToRender}
+      alt={alt}
+      onError={() => {
+        setSrcToRender(FALLBACK_AVATAR_URL);
+      }}
+    />
+  );
 };
 
 export default Avatar;
